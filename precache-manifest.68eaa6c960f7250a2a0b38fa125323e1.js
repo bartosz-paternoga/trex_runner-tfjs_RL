f9b3efb83aaf845505459f35f98b8e907a318dae
bartosz-paternoga <bpaternoga@hotmail.com>
2019-01-03 13:43:41 +0100

    Updates

---
+++ precache-manifest.68eaa6c960f7250a2a0b38fa125323e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a93388d5f909f6fc904",
+    "url": "/trex_runner-tfjs_RL/static/css/main.3b4724d5.chunk.css"
+  },
+  {
+    "revision": "0a93388d5f909f6fc904",
+    "url": "/trex_runner-tfjs_RL/static/js/main.0a93388d.chunk.js"
+  },
+  {
+    "revision": "c74cc11c0a215e93bf88",
+    "url": "/trex_runner-tfjs_RL/static/js/1.c74cc11c.chunk.js"
+  },
+  {
+    "revision": "6885241905af5bf5ce90",
+    "url": "/trex_runner-tfjs_RL/static/js/runtime~main.68852419.js"
+  },
+  {
+    "revision": "4769545300ce16d5e8d7636e6f77ca3b",
+    "url": "/trex_runner-tfjs_RL/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/trex_runner-tfjs_RL/precache-manifest.68eaa6c960f7250a2a0b38fa125323e1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/trex_runner-tfjs_RL/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
